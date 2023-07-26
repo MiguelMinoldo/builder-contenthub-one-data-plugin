afae0f292fc1328fc654f48e4edd3adeba92b6fe
Elakkuvan Rajamani <elakkuvan.rajamani@valtech.com>
2023-07-27 01:48:51 +0200

    Included Field Filtering in the Query window

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,8 +5,28 @@ import { getContentTypes } from "../lib/getContentTypes";
 import { getContentsByType } from "../lib/getContentsByType";
 
 const pluginId = pkg.name;
-const metaFields = ["version", "status", "type", "createdBy", "createdAt", "updatedBy", "updatedAt", "publishedBy", "publishedAt"];
-const chFieldTypes = ["Text", "Boolean", "Number", "Symbol", "Reference", "Media", "ShortText", "LongText" ]
+const metaFields = [
+  "version",
+  "status",
+  "type",
+  "createdBy",
+  "createdAt",
+  "updatedBy",
+  "updatedAt",
+  "publishedBy",
+  "publishedAt",
+];
+const chFieldTypes = [
+  "Text",
+  "Boolean",
+  "Number",
+  "Symbol",
+  "Reference",
+  "Media",
+  "ShortText",
+  "LongText",
+];
+const chFieldsTypesToExclude = ["Reference", "Media"];
 
 registerDataPlugin(
   {
@@ -19,19 +39,21 @@ registerDataPlugin(
         name: "clientId",
         type: "string",
         required: true,
-        helperText: "Get your Client ID from Content Hub ONE: Integration > OAuth > Client Credentials. https://doc.sitecore.com/ch-one/en/developers/content-hub-one/content-management-api--authentication.html",
+        helperText:
+          "Get your Client ID from Content Hub ONE: Integration > OAuth > Client Credentials. https://doc.sitecore.com/ch-one/en/developers/content-hub-one/content-management-api--authentication.html",
       },
       {
         name: "clientSecret",
         type: "string",
         required: true,
-        helperText: "Get your Client Secret from Content Hub ONE: Integration > OAuth > Client Credentials. https://doc.sitecore.com/ch-one/en/developers/content-hub-one/content-management-api--authentication.html",
+        helperText:
+          "Get your Client Secret from Content Hub ONE: Integration > OAuth > Client Credentials. https://doc.sitecore.com/ch-one/en/developers/content-hub-one/content-management-api--authentication.html",
       },
     ],
     ctaText: `Connect with your Content Hub One Tenant`,
   },
   // settings will be an Observable map of the settings configured above
-  async (settings: { get: (arg0: string) => string; }) => {
+  async (settings: { get: (arg0: string) => string }) => {
     var clientId = settings.get("clientId")?.trim();
     var clientSecret = settings.get("clientSecret")?.trim();
     const token = await getCHToken(clientId, clientSecret);
@@ -41,31 +63,32 @@ registerDataPlugin(
       async getResourceTypes() {
         const contentTypesCH1 = await getContentTypes(token);
         return contentTypesCH1.map((type) => {
-          const acceptableFields = type.fields.filter((field) => chFieldTypes.includes(field.type));
+          const acceptableFields = type.fields.filter((field) =>
+            chFieldTypes.includes(field.type)
+          );
           const chTypesFormatted = {
             name: type.name["en-US"], // This en-US based read, ll be changed
             id: type.id,
             canPickEntries: true,
             inputs: () => {
-              
               const fields: any = [
                 {
-                  name: 'Page Number',
-                  friendlName: 'Page Number',
+                  name: "Page Number",
+                  friendlName: "Page Number",
                   advanced: true,
                   defaultValue: 1,
                   min: 0,
                   max: 100,
-                  type: 'number',
+                  type: "number",
                 },
                 {
-                  name: 'Page Size',
-                  friendlName: 'Page Size',
+                  name: "Page Size",
+                  friendlName: "Page Size",
                   advanced: true,
                   defaultValue: 1,
                   min: 0,
                   max: 100,
-                  type: 'number',
+                  type: "number",
                 },
                 {
                   name: "Order By",
@@ -77,14 +100,34 @@ registerDataPlugin(
                       value: key,
                     }))
                     .concat(
-                      acceptableFields
-                        .map((field) => ({
-                          label: field.name["en-US"],
-                          value: field.id,
-                        }))
+                      acceptableFields.map((field) => ({
+                        label: field.name["en-US"],
+                        value: field.id,
+                      }))
                     ),
                 },
               ];
+              if (acceptableFields.length > 0) {
+                fields.push({
+                  name: "fields",
+                  advanced: true,
+                  type: "object",
+                  friendlyName: `${type.name["en-US"]} fields`,
+                  subFields: acceptableFields
+                    .filter(
+                      (field) => !chFieldsTypesToExclude.includes(field.type)
+                    )
+                    .map((field) => ({
+                      type:
+                        field.type === "Symbol"
+                          ? "text"
+                          : field.type.toLowerCase(),
+                      name: field.id,
+                      friendlyName: field.name["en-US"],
+                      helperText: `Query by a specific "${field.name["en-US"]}"" on ${field.type}`,
+                    })),
+                } as any);
+              }
               return fields;
             },
             toUrl: (options: any) => {
@@ -92,12 +135,16 @@ registerDataPlugin(
               if (options.entry) {
                 return `https://contenthub-one-builder-router.vercel.app/api/chonecontent/${options.entry}?clientId=${encriptedClientId}&clientSecret=${encriptedClientSecret}`;
               }
-              
-              let fields = (options.fields && Object.keys(options.fields).length > 0 && options.fields) || null;
-              
+
+              let fields =
+                (options.fields &&
+                  Object.keys(options.fields).length > 0 &&
+                  options.fields) ||
+                null;
+
               if (fields) {
                 fields = Object.keys(fields).reduce((acc, key) => {
-                  const omitValue = fields[key] === '';
+                  const omitValue = fields[key] === "";
                   return {
                     ...acc,
                     ...(omitValue ? {} : { [key]: fields[key] }),
@@ -105,14 +152,17 @@ registerDataPlugin(
                 }, {});
               }
 
-                // by query (TODO)
-                 return `https://contenthub-one-builder-router.vercel.app/api/chonecontent/${options.entry}?clientId=${encriptedClientId}&clientSecret=${encriptedClientSecret}`;
-              },
+              // by query (TODO)
+              return `https://contenthub-one-builder-router.vercel.app/api/chonecontent/${options.entry}?clientId=${encriptedClientId}&clientSecret=${encriptedClientSecret}`;
+            },
           };
           return chTypesFormatted;
         });
       },
-      async getEntriesByResourceType(id: any, options: { searchText: string; resourceEntryId: string; }) {
+      async getEntriesByResourceType(
+        id: any,
+        options: { searchText: string; resourceEntryId: string }
+      ) {
         const searchText = options?.searchText as string;
         const typeId = id;
         const entries = await getContentsByType(token, typeId);
@@ -121,7 +171,7 @@ registerDataPlugin(
         // specific entry...
         if (options?.resourceEntryId) {
           const entry = entries.find(
-            item => item.id === options.resourceEntryId
+            (item) => item.id === options.resourceEntryId
           );
           if (entry) {
             return [
@@ -133,12 +183,12 @@ registerDataPlugin(
           }
         }
         // search query...
-        else if (searchText != '') {
+        else if (searchText != "") {
           return entries
             .filter(({ name }) =>
               name.toLowerCase().includes(searchText?.toLowerCase())
             )
-            .map(entry => ({
+            .map((entry) => ({
               id: entry.id,
               name: entry.name,
             }));
